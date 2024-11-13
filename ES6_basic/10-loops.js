184fca10ae741ccc4bdac7ebb588d1b1fe7aa0e9
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-13 14:44:36 +0100

    disabled eslint and changed task 6

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
     const myArray = [];
     for (const value of array) {
--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function createEmployeesObject(departmentName, employees) {
     const company = {};
     company[departmentName] = employees;
