a0b03911027f81b3df0f327473c631f4f4e70802
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-14 11:19:58 +0100

    no point

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -4,10 +4,6 @@ import Building from "./5-building.js"
 export default class SkyHighBuilding extends Building {
     constructor(sqft, floors) {
         super(sqft);
-
-        if (typeof floors !== 'number') {
-            throw new TypeError('Floors must be a number');
-        }
         this._floors = floors;
     }
 
@@ -15,14 +11,7 @@ export default class SkyHighBuilding extends Building {
         return this._floors;
     }
 
-    set floors(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('Floors must be a number');
-        }
-        this._floors = value;
-    }
-
     evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors.`;
+        return `Evacuate slowly the ${this._floors} floors`;
     }
 }
