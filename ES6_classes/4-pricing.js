68a223093f505cd8effedde83cc44254c7d7ba13
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-14 10:50:44 +0100

    if value instance of currency

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -27,7 +27,7 @@ export default class Pricing {
     }
 
     set currency(value) {
-        if (!(currency instanceof Currency)) {
+        if (!(value instanceof Currency)) {
             throw new TypeError('Currency must be a currency');
         }
         this._currency = value;
