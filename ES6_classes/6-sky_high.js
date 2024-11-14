37cec7a887fdb774a2144286a15b7bee2517f6f4
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-14 11:16:14 +0100

    set floors too

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,16 +3,18 @@ import Building from "./5-building.js"
 
 export default class SkyHighBuilding extends Building {
     constructor(sqft, floors) {
-        super (sqft);
-
-        if (typeof floors !== 'number') {
-            throw new TypeError('Floors must be a number');
-        }
+        super(sqft);
         this._floors = floors;
     }
+
     get floors() {
         return this._floors;
     }
+
+    set floors(newFloors) {
+        this._floors = newFloors;
+    }
+
     evacuationWarningMessage() {
         return `Evacuate slowly the ${this._floors} floors.`;
     }
