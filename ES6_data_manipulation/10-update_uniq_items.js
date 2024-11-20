e60dd86c55bbf7ca8a430148b6fd426fee42ed26
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-20 11:19:44 +0100

    if we only want to buy 1 banana we turn it into 100

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function updateUniqueItems(shoppingList) {
+    if (!(shoppingList instanceof Map)) {
+        throw new Error('Cannot process');
+    }
+    for (const [key, value] of map) {
+        if (value === 1) {
+            map.set(key, 100);
+        }
+    }
+
+    return map;
+}
