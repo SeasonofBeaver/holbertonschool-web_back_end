b5706a1eb7ec456ef161bc3a6c7f7da3a199faa9
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-21 10:07:50 +0100

    added results

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,8 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-        .then(([photo, user]) => {
+        .then(([results]) => {
+            const [photo, user] = results;
             console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
         })
         .catch(() => {
