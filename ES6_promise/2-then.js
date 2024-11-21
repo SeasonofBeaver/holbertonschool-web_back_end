81886fc432b67f9f17d5db34bc7f602caae7606f
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-21 09:41:15 +0100

    promise with console return

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+    return Promise
+        .then(() => {
+            console.log('Got a response from the API');
+            return {
+                status: 200,
+                body: 'Success',
+            };
+        })
+        .catch(() => {
+            console.log('Got a response from the API');
+            return new Error();
+        });
+}
