cbf0c7f20e332c5121d261c70777d1bf4e4c71a3
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-21 10:51:19 +0100

    log in then too

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,6 +2,7 @@
 export default function handleResponseFromAPI(promise) {
     return promise
         .then(() => {
+            console.log('Got a response from the API');
             return {
                 status: 200,
                 body: 'Success',
