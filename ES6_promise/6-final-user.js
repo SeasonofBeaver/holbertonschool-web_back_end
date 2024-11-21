90e383568953f8f8237f29049a663e7fdbae1126
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-21 11:24:08 +0100

    changed as my first thought was not working

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    const signUpPromise = signUpUser(firstName, lastName).then(
-        (value) => ({ status: 'fulfilled', value }),
-        (reason) => ({ status: 'rejected', value: reason })
-    );
-    const uploadPhotoPromise = uploadPhoto(fileName).then(
-        (value) => ({ status: 'fulfilled', value }),
-        (reason) => ({ status: 'rejected', value: reason })
-    );
-
-    return Promise.allSettled([signUpPromise, uploadPhotoPromise]);
-}
+export default async (firstName, lastName, fileName) => {
+    const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+    return results.map((promise) => ({
+        status: promise.status,
+        value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+    }));
+};
