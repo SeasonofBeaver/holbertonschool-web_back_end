05e264cbe3c4da3f2a2906e6e49a8ecd3ff05357
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-21 10:43:52 +0100

    utils without .js in the end

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-        .then(([results]) => {
+        .then((results) => {
             const [photo, user] = results;
             console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
         })
