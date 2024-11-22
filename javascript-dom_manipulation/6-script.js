4f0e64189c876e5e5dcf8bd25ad871cc7fb007f9
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-22 10:33:53 +0100

    gets character from url

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector('#character').textContent = data.name;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
