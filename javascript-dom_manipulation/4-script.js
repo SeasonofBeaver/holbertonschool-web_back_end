93028081b27308c16c2035ceb668a2df5904d9c0
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-11-22 10:23:16 +0100

    add a list to a unsorted list

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+document.querySelector('#add_item').addEventListener('click', () => {
+    const ul = document.querySelector('.my_list');
+    const li = document.createElement('li');
+    li.textContent = 'Item';
+    ul.appendChild(li);
+});
