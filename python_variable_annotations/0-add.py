3b55e096c23ae6ad019bfb7ab6bf04d489ee0c79
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-03 14:33:22 +0100

    added blanc line

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 """An add function that combines 2 float numbers."""
+
+
 def add(a: float, b: float) -> float:
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 """A concat function that combines 2 strings."""
+
+
 def concat(a: str, b: str) -> str:
     return a + b
