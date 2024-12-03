755e5f4a2fdc0323081fcb58ebcf52d8c2bd496e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-03 14:40:41 +0100

    added more doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,4 +3,14 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return their sum.
+
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,4 +3,14 @@
 
 
 def concat(a: str, b: str) -> str:
+    """
+    Add two strings and return their combination.
+
+    Args:
+    a (string): The first number.
+    b (string): The second number.
+
+    Returns:
+    string: The combination of the 2 strings.
+    """
     return a + b
