935aff18844b4f7f5a54393e927a1f0cbd7985c7
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-03 14:43:21 +0100

    create floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+"""create a floor function that returns it from a number"""
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of the float variable.
+
+    Args:
+    n (float): The number.
+
+    Returns:
+    int: the floor of the variable.
+    """
+    return math.floor(n)
