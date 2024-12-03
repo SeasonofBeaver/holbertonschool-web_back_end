8326941a13725e385e8e53aac5b38ef057196812
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-03 14:55:48 +0100

    the string representation of the input number

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""create a string function that returns the string representation of the number input"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string representation of the float variable.
+
+    Args:
+    n (float): The number.
+
+    Returns:
+    str: the string representation of the variable.
+    """
+    return str(n)
