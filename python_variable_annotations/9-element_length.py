5b431ba5cf582df51344e16158180dd04e63e948
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-03 15:52:34 +0100

    changed to sequence and iterable

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """function for element length"""
 
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Return a list of tuples with an element of the list and its length.
 
     Args:
-    lst (List[str]): The list.
+    lst (Iterable[Sequence]): The list.
 
     Returns:
-    List[Tuple[str, int]]: A list of tuples where
+    List[Tuple[Sequence, int]]: A list of tuples where
     with an element of the list and its length.
     """
     return [(i, len(i)) for i in lst]
