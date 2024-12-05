92550bcb992076e04c11a599dbe1cdbc26a94cc2
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-05 14:09:50 +0100

    changed name of function and imported another task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,11 +5,11 @@ import asyncio
 from typing import List
 
 
-wait_random = __import__('1-concurrent_coroutines').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_random(n: int, max_delay: int) -> List[float]:
-    """ return a list of sorted delays """
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ returns a sorted list of delays """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
