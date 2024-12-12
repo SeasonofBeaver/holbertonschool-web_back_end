3a7d4586b2d49de0da263a02a8542ed7cb6d6dcf
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-12 11:52:19 +0100

    changed logs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 """ Stats about Nginx """
-
 from pymongo import MongoClient
 
 
-def nginx_stats():
-    """prints stats about Nginx logs"""
+def log_stats():
+    """ prints stats about Nginx logs """
+
     client = MongoClient('localhost', 27017)
     db = client.logs
     collection = db.nginx
-
+    
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    
     for method in methods:
-        methodCount = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {methodCount}")
-
+        methodcount = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {methodcount}")
+    
     statusCheck = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{statusCheck} status check")
 
-
 if __name__ == "__main__":
-    nginx_stats()
+    log_stats()
