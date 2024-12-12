89abbdb8f60f7f634506b4b9d25597af21533e1f
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-12-12 14:18:11 +0100

    changed again with the client and try accept

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,27 +4,24 @@
 from pymongo import MongoClient
 
 
-def nginx_stats() -> None:
+def log_stats():
     """prints stats about Nginx logs"""
-    try:
-        with MongoClient("mongodb://localhost:27017/") as client:
-            db = client.logs
-            collection = db.nginx
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
 
-            total_logs = collection.count_documents({})
-            print(f"{total_logs} logs")
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-            methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-            print("Methods:")
-            for method in methods:
-                method_count = collection.count_documents({"method": method})
-                print(f"\tmethod {method}: {method_count}")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-            status_check = collection.count_documents({"method": "GET", "path": "/status"})
-            print(f"{status_check} status check")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    nginx_stats()
+    log_stats()
